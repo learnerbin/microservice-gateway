8e0335a861d43952ad9d8b106f349cc363c2368b
Shabin-k <helloshabink@gmail.com>
2024-04-17 21:20:55 +0530

    learn gateway

---
+++ src/app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "corse";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+import services from "./config/index.js";
+import rateLimitAndTimeout from "./middleware/index.js";
+// Create an instance of Express app
+const app = express();
+
+// Middleware setup
+app.use(cors()); // Enable CORS
+app.use(helmet()); // Add security headers
+app.use(morgan("combined")); // Log HTTP requests
+app.disable("x-powered-by"); // Hide Express server information
+
+// Apply the rate limit and timeout middleware to the proxy
+app.use(rateLimitAndTimeout);
+
+// Set up proxy middleware for each microservice
+services.forEach(({ route, target }) => {
+  // Proxy options
+  const proxyOptions = {
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^${route}`]: "",
+    },
+  };
+  // Apply rate limiting and timeout middleware before proxying
+  app.use(route, rateLimitAndTimeout, createProxyMiddleware(proxyOptions));
+});
+
+// Handler for route-not-found
+app.use((_req, res) => {
+  res.status(404).json({
+    code: 404,
+    status: "Error",
+    message: "Route not found.",
+    data: null,
+  });
+});
+
+export default app;
---
+++ src/config/index.js
@@ -0,0 +1,19 @@
+export const services = [
+  {
+    route: "/auth",
+    target: "https://your-deployed-service.herokuapp.com/auth",
+  },
+  {
+    route: "/users",
+    target: "https://your-deployed-service.herokuapp.com/users/",
+  },
+  {
+    route: "/chats",
+    target: "https://your-deployed-service.herokuapp.com/chats/",
+  },
+  {
+    route: "/payment",
+    target: "https://your-deployed-service.herokuapp.com/payment/",
+  },
+  // Add more services as needed either deployed or locally.
+];
